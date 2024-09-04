132813ad578fc85b6d39ba2ef8f00504d8b466d7
BaptisLJ <loyal.baptist@unisys.com>
2024-09-04 13:34:30 +0530

    first commit

---
+++ src/main/java/com/example/springboot/jenkin/api/SpringBootJenkinsApplication.java
@@ -0,0 +1,23 @@
+package com.example.springboot.jenkin.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsApplication {
+	
+	public static Logger logger=LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+	
+	public void init() {
+		logger.info("Application started .....");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed .....");
+
+		SpringApplication.run(SpringBootJenkinsApplication.class, args);
+	}
+
+}
